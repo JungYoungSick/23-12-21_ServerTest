2083eaa5ebbfc806ec3e790c600e24c09b8905a8
JungYoungSick <junfas1216@gmail.com>
2023-12-19 23:52:45 +0900

    express를 이용하여 로그인 시 페이지가 바뀌는 실험작업 완료

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
-app.post('/submit', (req, res) => {
-  res.send('다음으로 연결')
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
-
+app.post('/submit', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index2.html'));
+});
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
 })
