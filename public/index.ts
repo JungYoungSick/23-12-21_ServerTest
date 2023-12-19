99497534404415b6a045f048a97aabbf4557d53f
JungYoungSick <junfas1216@gmail.com>
2023-12-19 11:34:43 +0900

    타입 스크립트 함수 선언

--- public/index.ts
+++ public/index.ts
@@ -1,5 +1,6 @@
-function sayHello(name: string): void {
-  console.log(`Hello, ${name}!`);
+function add(x: number, y: number): number {
+  return x + y;
 }
-const myName: string = "JUNG";
-sayHello(myName);
+const result = add(4, 2);
+
+console.log(result);
