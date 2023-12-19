0e33e9a5c98cf0dfae56afd65cdb703555d9c602
JungYoungSick <junfas1216@gmail.com>
2023-12-19 23:10:51 +0900

    express작성 완료

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  res.send("hello World!")
+})
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`)
+})
