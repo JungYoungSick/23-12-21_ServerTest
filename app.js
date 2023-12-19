f5de4fddb64c8f30961fc749833ef69d8ef4e896
JungYoungSick <junfas1216@gmail.com>
2023-12-20 00:15:50 +0900

    예제만들어서 연습, post 해시부분을 변경하여 페이지를 이동함

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require('express');
+const path = require('path')
 const app = express();
-const path = require('path');
 const port = 3000;
-const bodyParser = require('body-parser');
 
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
-app.post('/submit', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index2.html'));
 });
+
+app.post('/sub', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index2.html'));
+})
+
+
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
 })
