45634e4fc10e411a2ca7ed37f9de2060f3a94b59
JungYoungSick <junfas1216@gmail.com>
2023-12-21 00:14:27 +0900

    바닐라 자바스크립트를 이용한 post 데이터 받아오기 방식

--- app.js
+++ app.js
@@ -4,24 +4,43 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
   function serverErrorLog() {
     res.writeHead(500);
-    return res.end('서버 점검하시오')
+    return res.end('서버에 문제가 있습니다.');
   }
-  console.log("요청이 잘 됩니다.")
+
+  console.log("어떤 요청이 들어오는지 확인", "URL ->", req.url, "method ->", req.method);
+
   if (req.url === '/' && req.method === 'GET') {
     fs.readFile('./public/index.html', 'utf-8', (err, data) => {
       if (err) {
-        serverErrorLog()
+        serverErrorLog();
       }
-      res.writeHead(200, { 'content-type': 'text/html' });
+      res.writeHead(200, { 'Content-type': 'text/html' });
       res.end(data);
-    })
+    });
+  } else if (req.url === '/login' && req.method === 'POST') {
+    let body = '';
+
+    req.on('data', (chunk) => {
+      body += chunk;
+    });
+
+    req.on('end', () => {
+      // 여기에서 body에는 POST 데이터가 들어 있습니다.
+      console.log('Received POST data:', body);
+
+      // 여기에 로그인 처리 로직을 추가하세요.
+      // 예를 들어, body를 파싱하여 로그인 정보를 확인하고 응답을 구성할 수 있습니다.
+
+      res.writeHead(200, { 'Content-type': 'text/plain' });
+      res.end('Login successful!');
+    });
   } else {
     res.writeHead(404);
-    res.end('안되자나');
+    res.end('Not Found');
   }
 });
 
-const port = 5500;
-server.listen(port, () => {
-  console.log(`http://localhost:${port}`)
-})
+const PORT = 8080;
+server.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}/`);
+});
